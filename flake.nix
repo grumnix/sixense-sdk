37acf24c3cfe5e88c649831e62affa2d3b2e769c
Ingo Ruhnke <grumbel@gmail.com>
2023-05-23 20:41:06 +0200

    Update flake.nix to nixos-23.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = rec {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
